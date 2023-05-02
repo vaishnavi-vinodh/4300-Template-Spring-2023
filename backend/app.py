fixed similarity computation

backend/app.py
@@ -9,6 +9,7 @@ import math
 import re
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
 
 # ROOT_PATH for linking with all your files.
@@ -22,6 +23,11 @@ MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = "flavors"
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "kardashiandb"
+
+ALPHA = 0.7
+BETA = 0.98
+GAMMA = 0.28
+DELTA = 0.000000001
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
@@ -40,11 +46,69 @@ TIME_START_INDEX = 9
 TIME_END_INDEX = -2
 
 
-def process_input(query, results, time, diet, course, keywords, data_dict):
-    ranked = rank(query, results)
+def process_input(query_ingredients, query_keywords, results, time, diet, course):
+    # TODO: account for keywords being blank/needing to be weighted less
+    n_docs = len(results)
+    index_to_key, postings_key, popularity = read_csvs()
+    idf = compute_idf(postings_key, n_docs)
+    keyword_cossim = cossim(query_keywords, postings_key,
+                            idf, index_to_key, n_docs)
+    ingredient_jaccard = jaccard(query_ingredients, results)
+    sorted = rank(results, keyword_cossim, ingredient_jaccard, popularity)
+    ranked = [results[i] for i, _ in sorted]
     filtered_time = filter_time(ranked, time)
     filtered_diet = filter_diet(filtered_time, diet)
     return filter_course(filtered_diet, course)
+
+
+def rank(results, keyword_cossim, ingredient_jaccard, popularity):
+    scores = {}
+    # TODO: Check empty ingredients
+    print(keyword_cossim)
+    print(ingredient_jaccard)
+    key_list = [v for _, v in keyword_cossim]
+    ingr_list = [v for _, v in ingredient_jaccard]
+    print(key_list)
+    print(ingr_list)
+    for id in range(len(results)):
+        if id in keyword_cossim and id in ingr_list:
+            scores[id] = ALPHA * ingredient_jaccard[id][0] + GAMMA * \
+                keyword_cossim[id] + \
+                (DELTA * popularity[id])
+        elif id in keyword_cossim:
+            scores[id] = BETA * keyword_cossim[id] + \
+                (DELTA * popularity[id])
+        elif id in ingr_list:
+            scores[id] = BETA * ingredient_jaccard[id][0] + \
+                (DELTA * popularity[id])
+        else:
+            scores[id] = DELTA * popularity[id]
+    return sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+
+def read_csvs():
+    index_to_key = clean_dict((pd.read_csv('index_to_key.csv', usecols=[
+                               'index', 'word']).to_dict(orient="records")), 'index', 'word')
+    postings_key = clean_dict((pd.read_csv('postings_key.csv', usecols=[
+        'word', 'postings']).to_dict(orient="records")), 'word', 'postings')
+    popularity = {k: int(v['popularity']) for k, v in (pd.read_csv('popularity.csv', usecols=[
+        'name', 'popularity']).to_dict(orient="index")).items()}
+    return index_to_key, postings_key, popularity
+
+
+def string_to_list(str):
+    result = []
+    for element in re.findall('\(.*?\)', str):
+        element = element[1:-1].split(',')
+        result.append((int(element[0]), int(element[1])))
+    return result
+
+
+def clean_dict(list, col1, col2):
+    result = {}
+    for dict in list:
+        result[dict[col1]] = dict[col2]
+    return result
 
 
 def tokenize(text):
@@ -52,18 +116,73 @@ def tokenize(text):
     return re.findall(r'[a-z]+', text)
 
 
-def rank(query, results):
-    ranks = []
+def compute_doc_norms(postings, idf, n_docs):
+    norms = np.zeros(n_docs)
+    for word in postings:
+        if word in idf:
+            idf_i = idf[word]
+            post = string_to_list(postings[word])
+            for j, tf_ij in post:
+                norms[j] += (tf_ij * idf_i) ** 2
+
+    norms = np.sqrt(norms)
+    return norms
+
+
+def dot_products(query_word_counts, postings, idf):
+    doc_scores = {}
+    for word in query_word_counts:
+        if word in postings:
+            post = string_to_list(postings[word])
+            for posting in post:
+                w_ij = posting[1] * idf[word]
+                w_iq = query_word_counts[word] * idf[word]
+                doc_scores[posting[0]] = doc_scores.get(
+                    posting[0], 0) + w_ij * w_iq
+    return doc_scores
+
+
+def compute_idf(inv_idx, n_docs):
+    idf = {}
+    for word in inv_idx:
+        df = len(inv_idx[word])
+        term_idf = math.log2(n_docs/(1 + df))
+        idf[word] = term_idf
+    return idf
+
+
+def cossim(query, postings, idf, index_to_key, n_docs):
+    key_to_index = {v: k for k, v in index_to_key.items()}
+    results = []
+    query_word_counts = Counter(tokenize(query.lower()))
+    numerators = dot_products(query_word_counts, postings, idf)
+    query_norm = 0
+    for word in query_word_counts:
+        if word in key_to_index:
+            query_norm += (query_word_counts[word] * idf[word]) ** 2
+    query_norm = math.sqrt(query_norm)
+
+    doc_norms = compute_doc_norms(postings, idf, n_docs)
+    for doc_id in numerators:
+        num = numerators[doc_id]
+        doc_norm = doc_norms[doc_id]
+        score = num / (doc_norm * query_norm)
+        results.append((score, doc_id))
+    return results
+
+
+def jaccard(query, results):
+    result = []
     q = tokenize(query)
     query = set(q)
-    for res in results:
+    for id, res in enumerate(results):
         ing = tokenize(res['ingredients'])
         ingredients = set(ing)
         intersection = len((query).intersection(ingredients))
         union = (len(query) + len(ingredients)) - intersection
-        ranks.append((float(intersection) / (union*len(ing)*len(q)), res))
-    final = sorted(ranks, key=lambda x: x[0])
-    return [res for _, res in final][::-1]
+        result.append((float(intersection) / (union*len(ing)*len(q)), id))
+
+    return result
 
 
 def filter_time(results, time):
@@ -96,14 +215,14 @@ def filter_diet(results, diet):
     return filtered
 
 
-def sql_search(text, time, diet, course, keywords):
+def sql_search(query_ingredients, query_keywords, time, diet, course):
     query_sql = f"""SELECT name, image_url, description, diet, prep_time, ingredients, course, cuisine FROM recipes"""
     keys = ["name", "image_url", "description",
             "diet", "prep_time", "ingredients", "course", "cuisine"]
     data = mysql_engine.query_selector(query_sql)
     data_dict = [dict(zip(keys, i)) for i in data]
-    results = process_input(text, data_dict, time, diet,
-                            course, keywords, data_dict)
+    results = process_input(query_ingredients, query_keywords, data_dict, time, diet,
+                            course)
     return json.dumps(results)
 
 
@@ -114,12 +233,12 @@ def home():
 
 @ app.route("/episodes")
 def episodes_search():
-    text = request.args.get("ingredients")
-    keywords = request.args.get("keywords")
+    query_ingredients = request.args.get("ingredients")
+    query_keywords = request.args.get("keywords")
     time = request.args.get("time")
     diet = request.args.get("diet")
     course = request.args.get("course")
-    return sql_search(text, time, diet, course, keywords)
+    return sql_search(query_ingredients, query_keywords, time, diet, course)
 
 
 # app.run(debug=True)

backend/pre-processing.py
@@ -10,9 +10,11 @@ import re
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def tokenize(text):
     text = text.lower()
     return re.findall(r'[a-z]+', text)
+
 
 def build_inverted_index(data):
     inverted_index = {}
@@ -23,23 +25,27 @@ def build_inverted_index(data):
                 inverted_index[word].append((doc_id, counts[word]))
             else:
                 inverted_index[word] = [(doc_id, counts[word])]
-    with open('inv_ind_key.csv', 'w') as f:
+    with open('postings_key.csv', 'w') as f:
         for key in inverted_index.keys():
-            f.write("%s,%s\n"%(key, inverted_index[key]))
+            f.write("%s,%s\n" % (key, inverted_index[key]))
 
 
-def build_vectorizer(max_features = 5000, stop_words="english", max_df=0.8, min_df=5, norm='l2'):
-    vectorizer = TfidfVectorizer(max_features=max_features, stop_words=stop_words, max_df=max_df, min_df=min_df, norm=norm)
+def build_vectorizer(max_features=5000, stop_words="english", max_df=0.8, min_df=5, norm='l2'):
+    vectorizer = TfidfVectorizer(
+        max_features=max_features, stop_words=stop_words, max_df=max_df, min_df=min_df, norm=norm)
     return vectorizer
+
 
 def tfidf(recipes, field):
     n_feats = 5000
     tfidf_vec = build_vectorizer()
     doc_by_vocab = np.empty([len(recipes), n_feats])
-    doc_by_vocab = tfidf_vec.fit_transform([r[field] for r in recipes]).toarray()
+    doc_by_vocab = tfidf_vec.fit_transform(
+        [r[field] for r in recipes]).toarray()
     np.savetxt("key_matrix.csv", doc_by_vocab, delimiter=",")
-    index_to_vocab = {i:v for i, v in enumerate(tfidf_vec.get_feature_names())}
+    index_to_vocab = {i: v for i, v in enumerate(
+        tfidf_vec.get_feature_names())}
     with open('index_to_key.csv', 'w') as f:
         for key in index_to_vocab.keys():
-            f.write("%s,%s\n"%(key, index_to_vocab[key]))
+            f.write("%s,%s\n" % (key, index_to_vocab[key]))
     return doc_by_vocab